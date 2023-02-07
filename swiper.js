abe147ad5bca5382f87ff288a8b7ece2dc67d244
tangtang1b1b <a0981632653@gmail.com>
2023-02-07 22:23:53 +0800

    up4

---
+++ swiper.js
@@ -0,0 +1,10 @@
+    
+  var mySwiper = new Swiper ('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    }, 
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })        
